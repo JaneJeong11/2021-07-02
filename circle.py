7e4c4fb4db0494d0c27891fe8f1b1966c8ad8cba
JaneJeong11 <86812742+JaneJeong11@users.noreply.github.com>
2021-07-02 12:17:08 +0900

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,31 @@
+import turtle
+
+turtle.showturtle()
+turtle.write("파이썬에 오신 것을 환영합니다.")
+turtle.forward(100)
+turtle.right(90)
+turtle.color("red")
+turtle.forward(50)
+
+
+turtle.right(90)
+turtle.color("green")
+turtle.forward(100)
+
+turtle.right(45)
+turtle.forward(80)
+
+turtle.goto(0,50)
+turtle.penup()
+turtle.goto(50,-50)
+turtle.pendown()
+turtle.color("red")
+turtle.circle(50)
+
+            
+        
+             
+
+
+            
+            
---
+++ print.py
@@ -0,0 +1,22 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("파이썬에 오신 것을 환영합니다.")
+파이썬에 오신 것을 환영합니다.
+>>> print("파이썬은 재밌습니까?")
+파이썬은 재밌습니까?
+>>> print("Hello, world!")
+Hello, world!
+>>> 
+>>> print("배고프다")
+배고프다
+>>> today()
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    today()
+NameError: name 'today' is not defined
+>>> print(date())
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    print(date())
+NameError: name 'date' is not defined
+>>> 
